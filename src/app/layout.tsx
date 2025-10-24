5ff83f10905ecc9751ca04357b9a6267a7830ff3
Robson Junior <jrobsonjr16@gmail.com>
2025-10-24 09:35:04 -0300

    Add analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "TTPD x TLOAS Mashup",
@@ -18,6 +20,8 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
